989e8f31221dd2d6c414d5998ef6d4eeaa1044f4
SirJUST <theoperaking@yahoo.com>
2020-03-07 22:25:24 -0500

    Add Spell view that displays static data

---
+++ DndMagicLibrary/Controllers/SpellController.cs
@@ -0,0 +1,25 @@
+﻿using DndMagicLibrary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DndMagicLibrary.Controllers
+{
+    public class SpellController : Controller
+    {
+        // GET: Spell
+        [Route("/{id}")]
+        public ActionResult Spell(int id)
+        {
+            int level = id;
+            var model = new SpellViewModel
+            {
+                ClassName = "Bard",
+                Spells = Data.SpellsKnown.GetSpells("Bard")
+            };
+            return View(model);
+        }
+    }
+}
---
+++ DndMagicLibrary/ViewModels/SpellViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DndMagicLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DndMagicLibrary.ViewModels
+{
+    public class SpellViewModel
+    {
+        public string ClassName { get; set; }
+        public List<List<Spell>> Spells { get; set; }
+    }
+}
